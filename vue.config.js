40bc2fe6d24391c0413420990d158a60516f86e1
lcolok(JUB) <425311101@qq.com>
2019-03-27 18:36:25 +0800

    启动模板提纯

---
+++ vue.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  outputDir: 'docs',
+  publicPath: undefined,
+  assetsDir: 'assets',
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+  css: undefined
+}
