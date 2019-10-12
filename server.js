2439c7680bf3a42bd77d1840549089a6530f8926
lcolok(Home) <425311101@qq.com>
2019-10-12 22:09:53 +0800

    加入Prettier

--- server.js
+++ server.js
@@ -3,9 +3,9 @@
 var AV = require('leanengine');
 
 AV.init({
-  appId: process.env.LEANCLOUD_APP_ID,
-  appKey: process.env.LEANCLOUD_APP_KEY,
-  masterKey: process.env.LEANCLOUD_APP_MASTER_KEY
+	appId: process.env.LEANCLOUD_APP_ID,
+	appKey: process.env.LEANCLOUD_APP_KEY,
+	masterKey: process.env.LEANCLOUD_APP_MASTER_KEY,
 });
 
 // 如果不希望使用 masterKey 权限，可以将下面一行删除
@@ -17,14 +17,19 @@ var app = require('./app');
 // LeanEngine 运行时会分配端口并赋值到该变量。
 var PORT = parseInt(process.env.LEANCLOUD_APP_PORT || process.env.PORT || 3000);
 
-app.listen(PORT, function (err) {
-  console.log('Node app is running on port:', PORT);
+app.listen(PORT, function(err) {
+	console.log('Node app is running on port:', `http://127.0.0.1:${PORT}`);
 
-  // 注册全局未捕获异常处理器
-  process.on('uncaughtException', function(err) {
-    console.error('Caught exception:', err.stack);
-  });
-  process.on('unhandledRejection', function(reason, p) {
-    console.error('Unhandled Rejection at: Promise ', p, ' reason: ', reason.stack);
-  });
+	// 注册全局未捕获异常处理器
+	process.on('uncaughtException', function(err) {
+		console.error('Caught exception:', err.stack);
+	});
+	process.on('unhandledRejection', function(reason, p) {
+		console.error(
+			'Unhandled Rejection at: Promise ',
+			p,
+			' reason: ',
+			reason.stack,
+		);
+	});
 });
